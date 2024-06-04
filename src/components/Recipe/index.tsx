eb3543df226b5b425211e504dcae3b72be238afa
ANTONIO MARCOS MORTARI PESSOA <antonio.mmpessoa@senacsp.edu.br>
2024-06-03 22:10:48 -0300

    added Recipe component

---
+++ src/components/Recipe/index.tsx
@@ -0,0 +1,39 @@
+import { TouchableOpacity, TouchableOpacityProps, Text, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { styles } from './styles';
+
+interface IRecipeProps extends TouchableOpacityProps {
+    image: string;
+    minutes: number;
+    name: string
+}
+
+const Recipe = ({
+    image,
+    minutes,
+    name,
+    ...rest
+}: IRecipeProps) => {
+    return (
+        <TouchableOpacity
+            {...rest}
+        >
+
+            <ImageBackground
+                source={{uri: image}}
+                style={styles.image}
+            >
+                <LinearGradient colors={['rgba(0,0,0,0.5)', '#000']} style={styles.linear}>
+
+                    <Text style={styles.name} numberOfLines={1} lineBreakMode='tail'>{name}</Text>
+
+                    <Text style={styles.minutes}  numberOfLines={1} lineBreakMode='tail'>{minutes}</Text>
+
+                </LinearGradient>
+            </ImageBackground>
+
+        </TouchableOpacity>
+    );
+}
+
+export { Recipe };
